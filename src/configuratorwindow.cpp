022b6e250c109dd105b51b5e3b43d8b55065badb
Samuel Lourenço <16304376+samuelfmlourenco@users.noreply.github.com>
2024-06-01 16:28:55 +0100

    Add files via upload

--- src/configuratorwindow.cpp
+++ src/configuratorwindow.cpp
@@ -156,10 +156,23 @@ void ConfiguratorWindow::on_actionSerialGeneratorEnable_toggled(bool checked)
 // Implemented in version 3.0
 void ConfiguratorWindow::on_actionSerialGeneratorSettings_triggered()
 {
-    int errcnt = 0;
-    QString errstr;
     SerialGeneratorDialog serialGeneratorDialog(this);
-    serialGeneratorDialog.exec();  // Temporary
+    serialGeneratorDialog.setPrototypeSerialLineEditText(serialgen_.prototypeSerial());
+    serialGeneratorDialog.setDigitsCheckBox(serialgen_.replaceWithDigits());
+    serialGeneratorDialog.setUppercaseCheckBox(serialgen_.replaceWithUppercaseLetters());
+    serialGeneratorDialog.setLowercaseCheckBox(serialgen_.replaceWithLowercaseLetters());
+    if (serialGeneratorDialog.exec() == QDialog::Accepted) {  //
+        QString prototypeSerial = serialGeneratorDialog.prototypeSerialLineEditText();
+        bool digit = serialGeneratorDialog.digitsCheckBoxIsChecked();
+        bool upper = serialGeneratorDialog.uppercaseCheckBoxIsChecked();
+        bool lower = serialGeneratorDialog.lowercaseCheckBoxIsChecked();
+        if (!prototypeSerial.contains('?') || (digit == false && upper == false && lower == false)) {
+            QMessageBox::critical(this, tr("Error"), tr("The serial generator settings are invalid and will not be applied.\n\nPlease verify that the serial prototype string contains at least one wildcard character (?) and at least one replacement option is selected."));
+        } else {
+            serialgen_.setPrototypeSerial(prototypeSerial);
+            serialgen_.setReplaceMode(digit, upper, lower);
+        }
+    }
 }
 
 // Implemented in version 1.6
