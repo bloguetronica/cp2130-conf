598f8d9c9462dfb63dfb37dea6b82192416a851e
Samuel Lourenço <16304376+samuelfmlourenco@users.noreply.github.com>
2024-06-02 12:48:22 +0100

    Add files via upload

--- src/configuratorwindow.cpp
+++ src/configuratorwindow.cpp
@@ -169,8 +169,8 @@ void ConfiguratorWindow::on_actionSerialGeneratorSettings_triggered()
         bool digit = serialGeneratorDialog.digitsCheckBoxIsChecked();
         bool upper = serialGeneratorDialog.uppercaseCheckBoxIsChecked();
         bool lower = serialGeneratorDialog.lowercaseCheckBoxIsChecked();
-        if (!prototype.contains('?') || (digit == false && upper == false && lower == false)) {  // If the user entered invalid settings (i.e. the serial prototype string does not contain a wildcard character or no replacement option was selected)
-            QMessageBox::critical(this, tr("Error"), tr("The serial generator settings are invalid and will not be applied.\n\nPlease verify that the serial prototype string contains at least one wildcard character (?) and at least one replacement option is selected."));
+        if (!prototype.contains('?') || (digit == false && upper == false && lower == false)) {  // If the user entered invalid settings (i.e. the prototype serial number does not contain a wildcard character or no replacement option was selected)
+            QMessageBox::critical(this, tr("Error"), tr("The serial generator settings are not valid and will not be applied.\n\nPlease verify that the prototype serial number contains at least one wildcard character (?) and that at least one replacement option is selected."));
         } else {  // Valid settings
             serialgensetting_.serialgen.setPrototypeSerial(prototype);
             serialgensetting_.serialgen.setReplaceMode(digit, upper, lower);
