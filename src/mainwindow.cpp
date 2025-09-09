55683d10d65bb607b77cbef7abd55bd7cb32baa6
Samuel Lourenço <16304376+samuelfmlourenco@users.noreply.github.com>
2025-09-09 14:25:39 +0100

    Add files via upload

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,5 +1,5 @@
 /* CP2130 Configurator - Version 1.3.2 for Debian Linux
-   Copyright (c) 2021-2024 Samuel Lourenço
+   Copyright (c) 2021-2025 Samuel Lourenço
 
    This program is free software: you can redistribute it and/or modify it
    under the terms of the GNU General Public License as published by the Free
