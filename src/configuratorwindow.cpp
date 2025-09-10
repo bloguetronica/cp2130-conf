25a07e93327e2d06189e57b3d5d000b7bef4462c
Samuel Lourenço <16304376+samuelfmlourenco@users.noreply.github.com>
2025-09-10 14:42:24 +0100

    Add files via upload

--- src/configuratorwindow.cpp
+++ src/configuratorwindow.cpp
@@ -1052,7 +1052,7 @@ bool ConfiguratorWindow::showInvalidInput()
 
 // Checks for errors and validates device operations
 // Renamed and modified in version 1.3.2 (previous name was opCheck())
-void ConfiguratorWindow::validateOperation(const QString &operation, int errcnt, QString errstr)
+void ConfiguratorWindow::validateOperation(const QString &operation, int errcnt, QString errstr)  // The variable "errstr" is passed by value here, because it needs to be modified locally!
 {
     if (errcnt > 0) {
         err_ = true;
